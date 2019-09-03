9cf6651a2466fde7a57d0cd8210d6551d33dabde
Panny <pannismcmannis@gmail.com>
2019-09-03 23:18:05 +0100

    Basic setup

--- public/home.js
+++ public/home.js
@@ -1,1 +1,14 @@
 console.log('render home controller');
+
+new Vue({
+    el: '#root',
+    data: {
+        title: 'hello world',
+        description: 'testing 123'
+    },
+    methods: {
+        
+
+    }
+
+})
