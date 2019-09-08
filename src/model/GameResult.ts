7316fc5966699f77882021d37d34dc8aa9a478dd
panzors <pannismcmannis@gmail.com>
2019-09-08 12:52:49 +0100

    Adding more results to be used later

--- src/model/GameResult.ts
+++ src/model/GameResult.ts
@@ -1,6 +1,6 @@
 import {Card} from './Card';
 
-export type GameResponseType = "ok" | "broken";
+export type GameResponseType = "ok" | "broken" | "win" | "lose";
 
 export class GameResult{
     constructor(public Status: GameResponseType, public Deck: Card[]){
